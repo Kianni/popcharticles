5c348e3646be83fa9762ddafd3622cff79c22321
Kirill Nikolaev <nikolaeff.k.a@gmail.com>
2024-12-05 14:12:09 +0200

    POP-7 build a registration form

---
+++ public/js/registration_form.js
@@ -0,0 +1,18 @@
+document
+  .getElementById('registrationForm')
+  .addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const response = await fetch('/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    const result = await response.json();
+    alert(result.message);
+  });
