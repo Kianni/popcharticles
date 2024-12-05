7eaba75b55f30f6a28979a26264e386fc074cbee
Kirill Nikolaev <nikolaeff.k.a@gmail.com>
2024-12-05 15:16:08 +0200

    POP-50 replace user routes

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from 'express';
-import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
-import User from './models/User.js';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const port = 3000;
@@ -26,47 +25,8 @@ app.get('/', (req, res) => {
   res.sendFile('index.html', { root: 'public' });
 });
 
-// Route to serve the registration form
-app.get('/registration', (req, res) => {
-  res.sendFile('registration_form.html', { root: 'public' });
-});
-
-// Route to handle user registration
-app.post('/register', async (req, res) => {
-  const { username, password } = req.body;
-
-  // Server-side validation
-  const usernameRegex = /^[a-zA-Z_]+$/;
-  const passwordRegex = /^[0-9]{4,}$/;
-
-  if (!usernameRegex.test(username)) {
-    return res.status(400).json({ message: 'Username can only contain letters and underscores.' });
-  }
-
-  if (!passwordRegex.test(password)) {
-    return res.status(400).json({ message: 'Password must be at least 4 characters long and contain only numbers.' });
-  }
-
-  try {
-    // Check if the username already exists
-    const existingUser = await User.findOne({ username });
-    if (existingUser) {
-      return res.status(400).json({ message: 'Username already exists' });
-    }
-
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    // Create a new user
-    const newUser = new User({ username, password: hashedPassword });
-    await newUser.save();
-
-    res.status(200).json({ message: 'User registered successfully' });
-  } catch (err) {
-    console.error('Error during user registration:', err);
-    res.status(500).json({ message: 'Internal server error', error: err.message });
-  }
-});
+// Use user routes
+app.use(userRoutes);
 
 app.listen(port, () => {
   console.log(`Server running at http://127.0.0.1:${port}/`);
